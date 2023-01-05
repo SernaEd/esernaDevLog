80e16eac9bbf3099418e6dce0fb436b3c39d6127
Lalo Serna <eserna@grapheke.com>
2023-01-05 16:08:12 -0600

    Small Example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import GuestList from "./state/GuestList";
 import ReactDOM from "react-dom/client";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
@@ -10,6 +11,7 @@ const App = () => {
         <div>
             <GuestList/>
             <UserSearch/>
+            <EventComponent/>
         </div>
     );
 };
