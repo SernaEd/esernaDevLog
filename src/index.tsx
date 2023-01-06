ebd92ffb6f6b8832efc4b93109693612f5fe24a2
Lalo Serna <eserna@grapheke.com>
2023-01-06 00:37:34 -0600

    Fixed warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import GuestList from "./state/GuestList";
 import ReactDOM from "react-dom/client";
-// import UserSearch from "./state/UserSearch";
-import EventComponent from "./events/EventComponent";
 import UserSearch from "./refs/UserSearch";
 
 const el = document.getElementById("root");
@@ -10,9 +7,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
     return (
         <div>
-            {/*<GuestList/>*/}
             <UserSearch/>
-            {/*<EventComponent/>*/}
         </div>
     );
 };
