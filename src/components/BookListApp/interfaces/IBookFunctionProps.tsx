11446e140d5f48119936b8d2ecdf8be8e8b5b186
Lalo Serna <eserna@grapheke.com>
2024-12-30 01:23:09 -0600

    Remove unused components and associated styles
    Deleted multiple unused React components and their related styles to clean up the codebase. This includes `Author`, `Image`, `PageLinks`, `Services`, and more, along with their CSS rules, streamlining the project files for better maintainability.

--- src/components/BookListApp/interfaces/IBookFunctionProps.tsx
+++
@@ -1,9 +0,0 @@
-import {BookProps} from "./IBookProps";
-import React from "react";
-
-export interface BookFunctionProps extends BookProps {
-    onBookClick: (id: number) => void,
-    children?: React.ReactNode,
-
-    number: number,
-}
