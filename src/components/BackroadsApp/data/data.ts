752fe6291016d453253ad54a174b9605765ac1f9
Lalo Serna <eserna@grapheke.com>
2024-12-30 01:25:34 -0600

    Remove unused components, styles, and data from project
    Unused components, CSS styles, and data related to `BookListApp` and `BackroadsApp` were removed to improve code clarity and reduce maintenance overhead. These changes streamline the codebase by removing redundant or unnecessary code.

--- src/components/BackroadsApp/data/data.ts
+++ src/components/BackroadsApp/data/data.ts
@@ -3,12 +3,7 @@ import tourImg2 from '../images/tour-2.jpeg';
 import tourImg3 from '../images/tour-3.jpeg';
 import tourImg4 from '../images/tour-4.jpeg';
 
-export const pageLinks = [
-    {id: 1, href: '#home', text: ' home'},
-    {id: 2, href: '#about', text: ' about'},
-    {id: 3, href: '#services', text: ' services'},
-    {id: 4, href: '#tours', text: ' tours'},
-]
+
 
 export const socialLinks = [
     {id: 1, href: 'https://www.facebook.com', icon: "fab fa-facebook"},
@@ -16,11 +11,7 @@ export const socialLinks = [
     {id: 3, href: 'https://www.squarespace.com', icon: "fab fa-squarespace"},
 ]
 
-export const services = [
-    {id: 1, icon:'fas fa-wallet fa-fw', title:'saving money', text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
-    {id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
-    {id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
-]
+
 
 export const tours = [
     {
