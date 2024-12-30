05be74ffb809fabfa707cf2603075062b9a79444
Lalo Serna <eserna@grapheke.com>
2024-12-30 01:12:35 -0600

    Remove unused components and add Qodana configuration file.
    This commit deletes unused React components from the project to clean up the codebase and improve maintainability. Additionally, it introduces a Qodana configuration file for static code analysis, enabling better code quality checks in CI/CD pipelines.

--- src/components/BookListApp/containers/BookList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {Book} from "../Book";
-import {Button} from "@mui/material";
-import {EventExamples} from "../EventExamples";
-import { books } from "../data/booksData";
-
-
--- src/components/Tutorial/FirstComponent.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const FirstComponent: React.FC = () => {
-    return React.createElement('h1',{},'Hello World');
-};
-
